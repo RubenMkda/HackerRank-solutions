025ce627b7ba9f39cb479f3b27e93a2f750fb412
RubenMkda <83974135+RubenMkda@users.noreply.github.com>
2022-05-19 12:27:01 -0400

    Update Army-game.js

--- Mathematics/Army-game.js
+++ Mathematics/Army-game.js
@@ -1,27 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the 'gameWithCells' function below.
  *
@@ -48,20 +24,4 @@ function gameWithCells(n, m) {
   if (n % 2 !== 0 && m % 2 !== 0) {
     return (count = (n / 2 + 0.5) * (m / 2 + 0.5));
   }
-}
-
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const firstMultipleInput = readLine().replace(/\s+$/g, "").split(" ");
-
-  const n = parseInt(firstMultipleInput[0], 10);
-
-  const m = parseInt(firstMultipleInput[1], 10);
-
-  const result = gameWithCells(n, m);
-
-  ws.write(result + "\n");
-
-  ws.end();
 }
